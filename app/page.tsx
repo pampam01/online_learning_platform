5c084cde2b0740ba7e0a4699251b83122b20d1bd
pampam01 <emailhpwiko@gmail.com>
2023-10-27 21:26:47 +0700

    add slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 import CardComponent from "@/components/Card";
+import Slider from "@/components/Slider";
+import SliderBottom from "@/components/SliderBottom";
 
+import SliderMain from "@/components/SliderMain";
+
+const images = ["/images/a.jpg", "/images/b.jpg"];
 const Page = () => {
-  // return <CardComponent />;
+  return (
+    <>
+      <SliderMain images={images} />
+      <CardComponent />
+      <Slider />
+      <SliderBottom />
+    </>
+  );
 };
 export default Page;
