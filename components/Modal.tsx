17b08f8d6e878d996a91cda5a06bf6ad146f0574
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2023-10-31 23:21:26 +0700

    adding login modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -50,7 +50,7 @@ export default function Modal({
                 leaveTo="opacity-0 scale-95"
               >
                 {/* modal */}
-                <Dialog.Panel className="w-[20rem] max-w-md transform overflow-hidden rounded-2xl bg-white/30 backdrop-blur-sm p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-[20rem] max-w-md transform overflow-hidden rounded-2xl bg-white/50 backdrop-blur-sm p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-center text-gray-900"
