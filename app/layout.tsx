09955faa82009277d014d1cf219b1bb750053571
pampam01 <emailhpwiko@gmail.com>
2023-11-01 23:13:20 +0700

    add picture

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <div className="container">{children}</div>
+        <div className="container">
+          <>{children}</>
+          <Footer />
+        </div>
       </body>
     </html>
   );
