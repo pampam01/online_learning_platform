e218abb6d6b61059ca6fde0af8ae48b43a8ccdf2
pampam01 <emailhpwiko@gmail.com>
2023-11-02 00:24:54 +0700

    change main

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,12 @@ import SliderMain from "@/components/SliderMain";
 const images = ["/images/a.jpg", "/images/b.jpg"];
 const Page = () => {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div className="h-[100vh] mt-12">
-      <CardComponent />
-    </div>
-=======
-=======
-
->>>>>>> origin/main
     <>
       <SliderMain images={images} />
       <CardComponent />
       <Slider />
       <SliderBottom />
     </>
-<<<<<<< HEAD
->>>>>>> pampam
-=======
-
-
->>>>>>> origin/main
   );
 };
 export default Page;
